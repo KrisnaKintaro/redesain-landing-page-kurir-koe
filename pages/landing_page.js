f318e6cb0aec04fa918359ebea44e7a3ccf1e012
Krisna Kintaro <krisnakintaro1256@gmail.com>
2026-08-12 16:21:11 +0700

    component how it works sudah selesai

--- pages/landing_page.js
+++ pages/landing_page.js
@@ -3,18 +3,20 @@ async function renderLandingPage() {
   const heroHTML = await renderHero();
   const statsHTML = await renderStats();
   const servicesHTML = await renderServices();
+  const howItWorksHTML = await renderHowItWorks();
 
   return `
         <div id="view-landing-page">
             ${navbarHTML}
             ${heroHTML}
             ${statsHTML}
-            
-            <!-- Render komponen Services di bawah Stats -->
             ${servicesHTML}
             
+            <!-- Render komponen How It Works di bawah Services -->
+            ${howItWorksHTML}
+            
             <main id="main-content"> 
-                <!-- Semudah 1-2-3 (How It Works) nyusul di sini -->
+                <!-- Komponen Partnership/Gabung Mitra nyusul di sini -->
             </main>
         </div>
     `;
@@ -25,5 +27,6 @@ function initLandingPageLogic() {
   initNavbarMasterLogic();
   if (typeof initHeroLogic === "function") initHeroLogic();
   if (typeof initStatsLogic === "function") initStatsLogic();
-  if (typeof initServicesLogic === 'function') initServicesLogic();
+  if (typeof initServicesLogic === "function") initServicesLogic();
+  if (typeof initHowItWorksLogic === 'function') initHowItWorksLogic();
 }

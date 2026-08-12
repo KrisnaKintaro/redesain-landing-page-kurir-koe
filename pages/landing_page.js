ee1f2e83de7edb41ab2f03984cb22ad8ec09f3e4
Krisna Kintaro <krisnakintaro1256@gmail.com>
2026-08-12 16:02:00 +0700

    component services sudah selesai

--- pages/landing_page.js
+++ pages/landing_page.js
@@ -1,27 +1,29 @@
 async function renderLandingPage() {
-    const navbarHTML = await renderNavbarMaster();
-    const heroHTML = await renderHero(); 
-    const statsHTML = await renderStats();
+  const navbarHTML = await renderNavbarMaster();
+  const heroHTML = await renderHero();
+  const statsHTML = await renderStats();
+  const servicesHTML = await renderServices();
 
-    return `
+  return `
         <div id="view-landing-page">
             ${navbarHTML}
             ${heroHTML}
-            
-            <!-- Render komponen Stats tepat di bawah Hero -->
             ${statsHTML}
             
-            <main id="main-content" class="min-h-[100vh]"> 
-                <!-- Nanti komponen Services dll nyusul di sini -->
+            <!-- Render komponen Services di bawah Stats -->
+            ${servicesHTML}
+            
+            <main id="main-content"> 
+                <!-- Semudah 1-2-3 (How It Works) nyusul di sini -->
             </main>
         </div>
     `;
-    
 }
 
 function initLandingPageLogic() {
-    // Hidupkan logic khusus komponen yang ada di halaman ini
-    initNavbarMasterLogic();
-    if (typeof initHeroLogic === 'function') initHeroLogic();
-    if (typeof initStatsLogic === 'function') initStatsLogic(); 
+  // Hidupkan logic khusus komponen yang ada di halaman ini
+  initNavbarMasterLogic();
+  if (typeof initHeroLogic === "function") initHeroLogic();
+  if (typeof initStatsLogic === "function") initStatsLogic();
+  if (typeof initServicesLogic === 'function') initServicesLogic();
 }
